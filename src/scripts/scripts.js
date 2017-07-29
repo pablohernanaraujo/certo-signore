876a4dd1e0563eb46203a5ec093dbd30c7f74f58
pablohernanaraujo <pablohernanaraujo@gmail.com>
2017-07-29 16:17:11 -0300

    'build'

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -3,4 +3,20 @@ $( document ).ready(function(){
     $(".button-collapse").sideNav();
     $('.slider').slider();
     $('.parallax').parallax();
+
+    $('a[href*=#]').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+        && location.hostname == this.hostname) {
+        var $target = $(this.hash);
+        $target = $target.length && $target
+        || $('[name=' + this.hash.slice(1) +']');
+        if ($target.length) {
+            var targetOffset = $target.offset().top;
+            $('html,body')
+            .stop().animate({scrollTop: targetOffset}, 1300);
+        return false;
+        }
+        }
+    });
+
 });
